2b6b6ec252dfc788e503506b12eaedf3cb02c505
Sai Lavu <sai@systempartners.com>
2012-01-11 15:33:44 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Directlogin::Application.config.secret_token = '224876fb10574964f2d7010063bc05d88e22ede3e5a684a661f729facf948b65be58077aa72a30128f78e890a4e4c0192d944426dd57fd8a419b6bec6ec8497b'
